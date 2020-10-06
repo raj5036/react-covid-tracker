6ad54225e7c375cdf763bf036e7e5f23a0c1242e
Raj <97rajmath@gmail.com>
2020-10-06 11:12:09 -0500

    Updates

---
+++ precache-manifest.924def83e9ebc9519cd5bfa877fcbbd1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9de3049bc730067c385b40d0554a91a4",
+    "url": "/react-covid-tracker/index.html"
+  },
+  {
+    "revision": "b8e12eba438698910683",
+    "url": "/react-covid-tracker/static/css/2.7c35e809.chunk.css"
+  },
+  {
+    "revision": "1b99f279577991ef9197",
+    "url": "/react-covid-tracker/static/css/main.02729728.chunk.css"
+  },
+  {
+    "revision": "b8e12eba438698910683",
+    "url": "/react-covid-tracker/static/js/2.855e3be4.chunk.js"
+  },
+  {
+    "revision": "68eb484dce045119a52132a8eb902e52",
+    "url": "/react-covid-tracker/static/js/2.855e3be4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1b99f279577991ef9197",
+    "url": "/react-covid-tracker/static/js/main.bccd9f82.chunk.js"
+  },
+  {
+    "revision": "261fd4b71819be31f984",
+    "url": "/react-covid-tracker/static/js/runtime-main.803e8507.js"
+  }
+]);
